67eecbaeb9c6a6a3114f965b255632d48455bd24
Ghiffari Kemal <ghiffarikemal@Ghiffaris-MacBook-Air.local>
2021-03-07 12:05:06 +0700

    First commit

---
+++ src/config/FIREBASE/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCTWZKZgMPy36nTY5ZXXq7344erbOOMXDA",
+    authDomain: "react-native-crud-3811c.firebaseapp.com",
+    projectId: "react-native-crud-3811c",
+    storageBucket: "react-native-crud-3811c.appspot.com",
+    messagingSenderId: "998571778331",
+    appId: "1:998571778331:web:c908ac446b0013a35f73be"
+})
+
+const FIREBASE = firebase;
+
+export default FIREBASE;
